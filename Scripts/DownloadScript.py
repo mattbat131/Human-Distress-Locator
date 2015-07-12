ed022fab08ed09f11ad32aa374381711eec229d5
Matthew Stern <mattbat131@gmail.com>
2015-07-11 22:27:05 -0400

    Update download

--- Scripts/DownloadScript.py
+++ Scripts/DownloadScript.py
@@ -43,14 +43,17 @@ def exists(idList, testId):
  
 def createARFF():
 	f = open("HumanDistress.arff".format(i), 'w')
-	f.write("@RELATION sounds\n @ATTRIBUTE class\t {Human-In-Distress, Other}\n @DATA\n")
+	f.write("@RELATION sounds\n @ATTRIBUTE bitrate\t NUMERIC\n @ATTRIBUTE bitdepth\t NUMERIC\n @ATTRIBUTE duration\t NUMERIC\n NUMERIC\n @ATTRIBUTE class\t {Human-In-Distress, Other}\n @DATA\n")
 	for jF in id_result:
+		f.write("{0},{1},{2},".format(jF['bitrate'], jF['bitdepth'], jF['duration']))
+		analysis = json.loads(curl(jF['analysis_frames']), encoding='iso-8859-1')
+		#f.write("
 		if "human" in jF["tags"] and ("distress" in jF["tags"] or "crying" in jF["tags"]    or "pain" in jF["tags"] or "screaming" in jF["tags"] or "moaning" in jF["tags"] or "scared" in jF["tags"] or "yelling" in jF["tags"]):
 			#write to file under Human-In-Distress
-			f.write("stuff")
+			f.write("Human-In-Distress\n")
 		else:
 			#write to file under Other
-			f.write("stuff")
+			f.write("Other\n")
 
 
 #Gets .json file of all sounds with same tags
