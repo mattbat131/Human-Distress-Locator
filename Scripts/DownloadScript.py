98d700a03d8ae39c039ab5eaf84a5c5642924b3d
Matthew Stern <mattbat131@gmail.com>
2015-07-11 21:32:29 -0400

    Update download

--- Scripts/DownloadScript.py
+++ Scripts/DownloadScript.py
@@ -4,9 +4,14 @@ import pycurl
 from io import BytesIO
 import json
 
-tag = input('Enter Tag: ')
+num = input('How many tags? ')
+tag = list()
+for i in range(int(num)):
+	n = input("Tag: ")
+	tag.append(n)
 key = input('Enter Key: ')
-file = input('JSON File: ')
+
+#file = input('JSON File: ')
 
 def runCommand():
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
@@ -22,34 +27,40 @@ def curl(u):
     c.perform()
     c.close()
     body = buffer.getvalue()
-    return body
+    return body.decode('iso-8859-1')
 
 def getId():
-    id = [i['id'] for i in json_result['results']]
+    id = list()
+    for j in range(int(num)):
+   	 id += [i['id'] for i in json_result[j]['results']]
     return id
 
-def createJson(ids, u):
-    for i in ids:
-        buffer = BytesIO()
-        body = curl(u)
-        f = open("{0}.json".format(i), 'w')
-        f.write(
+def exists(idList, testId):
+   for i in idList:
+       if i == testId:
+           return True
+   return False
     
-#Creates .json file of all sounds with same singular tag
-url = 'http://www.freesound.org/apiv2/search/text/?query={0}&token={1}'.format(tag, key)
-json = curl(url)
+#Gets .json file of all sounds with same tags
+json_result = list()
+for i in range(int(num)):
+	url = 'http://www.freesound.org/apiv2/search/text/?query={0}&token={1}'.format(tag[i], key)
+	json_result.append(json.loads(curl(url), encoding ='iso-8859-1'))
 
-
-#Gets id's of all sounds from .json
-json_result = json.loads(body.decode('iso-8859-1'))
+#Gets id's of all sounds from tag searches	
 allId = getId()
 
-#Gets .json for each sound 
-url = 'https://www.freesound.org/apiv2/sounds/{0}/analysis/?token={1}'.format(i, key)
-createJson(allId, url)    
+#Gets all .json files from ids
+blackListId = list()
+id_result = ''
+for i in allId:
+	if exists(blackListId, i) == False:
+		print(i)
+		url = 'https://www.freesound.org/apiv2/sounds/{0}/analysis/?token={1}'.format(i, key)
+		id_result += (json.loads(curl(url), encoding='iso-8859-1'))
+	blackListId.append(i)
 
-#Create .aarf file
-#convertJsonToAarf()
-
+print(id_result)
+#Creates .ARFF file
 #f = open("{0}.json".format(i), 'w')
 #f.write(idInfo.decode('iso-8859-1'))
