7563f360b23266dbde6ddfb3050dc3faa8353b06
Trevor Knight <trevorknight@gmail.com>
2015-11-01 13:49:29 -0500

    Feature selected and experiments

--- Scripts/new_knn.py
+++ Scripts/new_knn.py
@@ -15,14 +15,14 @@ def default_get_class_function(neighbor):
     return neighbor.data[-1]
 
 
-def find_two_class_labels(neighbors, get_class_function):
-    a_class = get_class_function(neighbors[0])
-    b_class = a_class
-    for neighbor in neighbors:
-        if get_class_function(neighbor) is not a_class:
-            b_class = get_class_function(neighbor)
-            break
-    return a_class, b_class
+# def find_two_class_labels(neighbors, get_class_function):
+#     a_class = get_class_function(neighbors[0])
+#     b_class = a_class
+#     for neighbor in neighbors:
+#         if get_class_function(neighbor) is not a_class:
+#             b_class = get_class_function(neighbor)
+#             break
+#     return a_class, b_class
 
 
 def euclidean_distance_function(one, two):
@@ -34,7 +34,7 @@ def euclidean_distance_function(one, two):
     return sqrt(squared_distance)
 
 
-def average_filtered_mode(top_neighbors, get_class_function):
+def average_filtered_mode(top_neighbors, get_class_function, classes, positive_class_threshold):
     sum = 0
     for neighbor in top_neighbors:
         sum += neighbor.distance
@@ -45,24 +45,22 @@ def average_filtered_mode(top_neighbors, get_class_function):
     return mode_based_function(avg_top_neighbors, get_class_function)
 
 
-# Assumes class labels are consistent and binary!!!!!!
-def weighted_average_class(top_neighbors, get_class_function):
+# Assumes class labels are binary!!!!!!
+def weighted_average_class(top_neighbors, get_class_function, classes, positive_class_threshold):
     weights = [1.0/n.distance for n in top_neighbors if n.distance != 0]
     weight_sum = sum(weights)
-    a_class, b_class = find_two_class_labels(top_neighbors, get_class_function)
-    if a_class is b_class:
-        return a_class
+    a_class, b_class = classes
     a_class_total = 0
     for i in range(len(top_neighbors)):
-        if get_class_function(top_neighbors[i]) is a_class:
+        if a_class in get_class_function(top_neighbors[i]):
             a_class_total += weights[i]
-    if a_class_total / weight_sum > 0.5:
+    if a_class_total / weight_sum > positive_class_threshold:
         return a_class
     else:
         return b_class
 
 
-def weighted_euclidian_distance_mean_class(top_neighbors, get_class_function):
+def weighted_euclidian_distance_mean_class(top_neighbors, get_class_function, classes, positive_class_threshold):
     weights = [n.distance**2 for n in top_neighbors]
     weight_sum_sqrt = sqrt(sum(weights))
     a_class, b_class = find_two_class_labels(top_neighbors, get_class_function)
@@ -70,7 +68,7 @@ def weighted_euclidian_distance_mean_class(top_neighbors, get_class_function):
         return a_class
     a_class_total = 0
     for i in range(len(top_neighbors)):
-        if get_class_function(top_neighbors[i]) is a_class:
+        if a_class in get_class_function(top_neighbors[i]):
             a_class_total += weights[i]
     if a_class_total / weight_sum_sqrt > .5:
         return a_class
@@ -78,7 +76,7 @@ def weighted_euclidian_distance_mean_class(top_neighbors, get_class_function):
         return b_class
 
 
-def weighted_sqr_mean_class(top_neighbors, get_class_function):
+def weighted_sqr_mean_class(top_neighbors, get_class_function, classes, positive_class_threshold):
     weights = [sqrt(n.distance) for n in top_neighbors]
     weight_sum = sum(weights)
     a_class, b_class = find_two_class_labels(top_neighbors, get_class_function)
@@ -86,7 +84,7 @@ def weighted_sqr_mean_class(top_neighbors, get_class_function):
         return a_class
     a_class_total = 0
     for i in range(len(top_neighbors)):
-        if get_class_function(top_neighbors[i]) is a_class:
+        if a_class in get_class_function(top_neighbors[i]):
             a_class_total += weights[i]
     if a_class_total / weight_sum > .5:
         return a_class
@@ -94,7 +92,7 @@ def weighted_sqr_mean_class(top_neighbors, get_class_function):
         return b_class
 
 
-def mode_based_function(top_neighbors, get_class_function):
+def mode_based_function(top_neighbors, get_class_function,  classes, positive_class_threshold):
     classes = [get_class_function(neighbor) for neighbor in top_neighbors]
     try:
         return mode(classes)
@@ -106,11 +104,11 @@ def mode_based_function(top_neighbors, get_class_function):
 #     for i in len()
 #     mean(top_neighbors)
 
-
-def k_nearest_neighbor(query, folds, k,
+# Classes should be in order of preference (for true positive/false positive weighting)
+def k_nearest_neighbor(query, folds, k, classes, positive_class_threshold,
                        distance_function=euclidean_distance_function,
                        get_class_function=default_get_class_function,
-                       compute_winning_class_function=weighted_sqr_mean_class):
+                       compute_winning_class_function=weighted_average_class):
     all_neighbors_heap = []
     for fold in folds:
         for point in fold:
@@ -118,4 +116,5 @@ def k_nearest_neighbor(query, folds, k,
             new_neighbor = neighbor_item(point, distance)
             heapq.heappush(all_neighbors_heap, new_neighbor)
     top_neighbors = [heapq.heappop(all_neighbors_heap) for i in range(k)]
-    return compute_winning_class_function(top_neighbors, get_class_function)
+    return compute_winning_class_function(top_neighbors, get_class_function,
+                                          classes, positive_class_threshold)
--- Scripts/new_main.py
+++ Scripts/new_main.py
@@ -7,7 +7,7 @@ from itertools import zip_longest
 
 
 def load_and_shuffle():
-    file = codecs.open("HumanDistress_Normalize_clean2.arff", 'rb', 'utf-8')
+    file = codecs.open("HumanDistress_Normalize_clean3_featureSelected.arff", 'rb', 'utf-8')
     arff_everything = arff.load(file)
     data_set = arff_everything['data']
     random.shuffle(data_set)
@@ -33,9 +33,16 @@ def report_accuracy(all_results):
     print(all_results)
 
 
+def frange(x, y, jump):
+  while x < y:
+    yield x
+    x += jump
+
+
 def main():
     data = load_and_shuffle()
     k_folds = 10
+    classes = ('Human', 'Other')
     folds = create_k_folds(data, k_folds)
     all_results = dict()
     all_results['true_pos'] = 0
@@ -43,35 +50,43 @@ def main():
     all_results['true_neg'] = 0
     all_results['false_neg'] = 0
 
-    for k in range(1, 21):
-        for i in range(len(folds)):
-            fold_under_test = folds[i]
-            folds_in_database = folds[:i] + folds[i+1:]
-            for point in fold_under_test:
-                result = new_knn.k_nearest_neighbor(point, folds_in_database, k)
-                #print(result, point[-1])
-                if is_positive_class(result):
-                    if is_positive_class(point[-1]):
-                        all_results['true_pos'] += 1
-                        #print('true_pos')
-                    else:
-                        all_results['false_pos'] += 1
-                        #print('false_pos')
+    for train_or_test in range(2):
+        if train_or_test == 0:
+            t_folds = folds[:-1]
+            print("Training")
+        else:
+            t_folds = [folds[-1]]
+            print("Testing")
+        for positive_class_threshold in frange(0.1, 0.451, 0.05):
+            k = 5
+
+            for i in range(len(t_folds)):
+                #print(i)
+                if train_or_test == 0:
+                    fold_under_test = t_folds[i]
+                    folds_in_database = t_folds[:i] + t_folds[i+1:]
                 else:
-                    if is_positive_class(point[-1]):
-                        all_results['false_neg'] += 1
-                        #print('false_neg')
+                    fold_under_test = t_folds[i]
+                    folds_in_database = folds[:-1]
+                for point in fold_under_test:
+                    result = new_knn.k_nearest_neighbor(point, folds_in_database, k, classes, positive_class_threshold)
+                    if is_positive_class(result):
+                        if is_positive_class(point[-1]):
+                            all_results['true_pos'] += 1
+                        else:
+                            all_results['false_pos'] += 1
                     else:
-                        all_results['true_neg'] += 1
-                        #print('true_neg')
-            # all_results["result"].append(result)
-            # all_results["actual"].append(point[-1])
-        print("k = ", k)
-        report_accuracy(all_results)
-        all_results['true_pos'] = 0
-        all_results['false_pos'] = 0
-        all_results['true_neg'] = 0
-        all_results['false_neg'] = 0
+                        if is_positive_class(point[-1]):
+                            all_results['false_neg'] += 1
+                        else:
+                            all_results['true_neg'] += 1
+            print("k = ", k)
+            print("positive class threshold =", positive_class_threshold)
+            report_accuracy(all_results)
+            all_results['true_pos'] = 0
+            all_results['false_pos'] = 0
+            all_results['true_neg'] = 0
+            all_results['false_neg'] = 0
 
 
 
